c0d9b0201983fa42c965e7a6d3fed2cb076b8014
Feng Gao <gaofeng2@msu.edu>
2019-08-23 00:39:22 -0400

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -175,3 +175,28 @@ def shuffled(index,norm_feature,Y):
         new_feature.append(norm_feature[i])
         new_Y.append(Y[i])
     return (new_feature,new_Y)
+
+
+
+def Kfold(length,fold):
+    size = np.arange(length).tolist()
+    train_index = []
+    val_index = []
+    rest = length % fold
+    fold_size = int(length/fold)
+    temp_fold_size = fold_size
+    for i in range(fold):
+        temp_train = []
+        temp_val = []
+        if rest>0:
+            temp_fold_size = fold_size+1
+            rest = rest -1
+            temp_val = size[i*temp_fold_size:+i*temp_fold_size+temp_fold_size]
+            temp_train = size[0:i*temp_fold_size] + size[i*temp_fold_size+temp_fold_size:]
+        else:
+            temp_val = size[(length % fold)*temp_fold_size+(i-(length % fold))*fold_size
+                            :(length % fold)*temp_fold_size+(i-(length % fold))*fold_size+fold_size]
+            temp_train = size[0:(length % fold)*temp_fold_size+(i-(length % fold))*fold_size] + size[(length % fold)*temp_fold_size+(i-(length % fold))*fold_size+fold_size:]
+        train_index.append(temp_train)
+        val_index.append(temp_val)
+    return (train_index,val_index)
